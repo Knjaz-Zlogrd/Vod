9b8168cd4410d6b4f860d410f169464d8a062aa8
Mihajlo Mulic <mulic92zr@gmail.com>
2019-08-15 03:49:59 +0200

    starting version

---
+++ Vod/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vod.Startup))]
+namespace Vod
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
