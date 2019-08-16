63743186835493952eb4057eeb201d550e2e3020
Mihajlo Mulic <mulic92zr@gmail.com>
2019-08-16 16:09:51 +0200

    added CustomersController and changed _NavBar

--- Vod/Controllers/MoviesController.cs
+++ Vod/Controllers/MoviesController.cs
@@ -36,17 +36,22 @@ namespace Vod.Controllers
             return Content("id=" +id );
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ViewResult Index()
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+            var movies = GetMovies();
 
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex, sortBy));
+            return View(movies);
         }
-        
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
