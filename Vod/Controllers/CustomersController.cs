dfca3d4a1d3fccf83e813ca6c0f893039b79f9c8
Mihajlo Mulic <mulic92zr@gmail.com>
2019-08-23 02:36:15 +0200

    added _context.SaveChanges to CustomersController

--- Vod/Controllers/CustomersController.cs
+++ Vod/Controllers/CustomersController.cs
@@ -22,6 +22,26 @@ namespace Vod.Controllers
         {
             _context.Dispose();
         }
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
         public ViewResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
@@ -37,17 +57,6 @@ namespace Vod.Controllers
                 return HttpNotFound();
 
             return View(customer);
-        }
-
-        public ActionResult New()
-        {
-            var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
-            {
-                MembershipTypes = membershipTypes
-            };
-
-            return View(viewModel);
         }
             
             
