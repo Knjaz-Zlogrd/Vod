b090aa9cee153ef8b893550512990011ccdcdc88
Mihajlo Mulic <mulic92zr@gmail.com>
2019-08-23 02:51:23 +0200

    added edit form for customers

--- Vod/Controllers/CustomersController.cs
+++ Vod/Controllers/CustomersController.cs
@@ -26,12 +26,12 @@ namespace Vod.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
             };
 
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
@@ -59,7 +59,21 @@ namespace Vod.Controllers
             return View(customer);
         }
             
-            
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel()
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
 
     }
 }
