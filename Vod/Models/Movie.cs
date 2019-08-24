97a5c7d366aaf3ba5e8973ae693732819231f0ef
Mihajlo Mulic <mulic92zr@gmail.com>
2019-08-24 02:20:28 +0200

    added save/edit form to movies

--- Vod/Models/Movie.cs
+++ Vod/Models/Movie.cs
@@ -8,7 +8,6 @@ namespace Vod.Models
 {
     public class Movie
     {
-        [Required]
         public Genre Genre { get; set; }
 
         public int Id { get; set; }
@@ -17,12 +16,16 @@ namespace Vod.Models
         [StringLength(255)]
         public string Name { get; set; }
 
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
         public DateTime DateAdded { get; set; }
 
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
 
+        [Required]
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
 
     }
